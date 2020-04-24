2a906fa980964cf5a36e449546f17aeb2cd83ca3
aesopsfoxfables <64263185+aesopsfoxfables@users.noreply.github.com>
2020-04-24 07:29:31 -0500

    css images and js

---
+++ javascript/foxcrow/script1.js
@@ -0,0 +1,14 @@
+var animation = anime({
+  targets: '.crowfly',
+  translateY: 100,
+  translateX: -200,
+  elasticity: 200,
+  easing: 'linear',
+  autoplay: false
+});
+
+var seekProgress = document.querySelector('.progress');
+
+seekProgress.oninput = function() {
+  animation.seek(animation.duration * (seekProgress.value / 100));
+};
